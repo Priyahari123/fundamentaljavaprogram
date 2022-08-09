1ffd1e146a301efd0d7910987d2f3329ee050acd
Priyahari123 <haripriyasanthiya20@gmail.com>
2022-08-09 17:11:56 +0530

    haripriya

---
+++ src/main/java/BasicJavaProgram/java/App.java
@@ -0,0 +1,13 @@
+package BasicJavaProgram.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
